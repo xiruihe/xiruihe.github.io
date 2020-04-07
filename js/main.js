e22c99efcd8a45b1fa5204acca26dc8c40b633cb
Xirui He <xih173@ucsd.edu>
2020-04-06 23:24:01 -0700

    contact

--- js/main.js
+++ js/main.js
@@ -265,3 +265,125 @@
 	
 
 }());
+
+class TypeWriter {
+	constructor(txtElement, words, wait = 200) {
+		this.txtElement = txtElement;
+		this.words = words;
+		this.txt = '';
+		this.wordIndex = 0;
+		this.wait = parseInt(wait, 10);
+		this.type();
+		this.isDeleting = false;
+	}
+
+	type() {
+		// Current index of word
+		const current = this.wordIndex % this.words.length;
+		// Get full text of current word
+		const fullTxt = this.words[current];
+
+		// Check if deleting
+		if(this.isDeleting) {
+			// Remove char
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			// Add char
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		// Insert txt into element
+		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+		// Initial Type Speed
+		let typeSpeed = 100;
+
+		if(this.isDeleting) {
+			typeSpeed /= 2;
+		}
+
+		// If word is complete
+		if(!this.isDeleting && this.txt === fullTxt) {
+			// Make pause at end
+			typeSpeed = this.wait;
+			// Set delete to true
+			this.isDeleting = true;
+		} else if(this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			// Move to next word
+			this.wordIndex++;
+			// Pause before start typing
+			typeSpeed = 500;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	}
+}
+
+/ Init On DOM Load
+//document.addEventListener('DOMContentLoaded', init);
+
+document.addEventListener("DOMContentLoaded", function() {
+	fields.name = document.getElementById('Name');
+	fields.email = document.getElementById('email');
+	fields.subject = document.getElementById('subject');
+	fields.message = document.getElementById('message');
+})
+
+function isNotEmpty(value) {
+	if (value == null || typeof value == 'undefined' ) return false;
+	return (value.length > 0);
+}
+
+function isEmail(email){
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+	if (field == null) return false;
+
+	let isFieldValid = validationFunction(field.value)
+	if (!isFieldValid) {
+		field.className = 'placeholderRed';
+	} else {
+		field.className = '';
+	}
+
+	return isFieldValid;
+}
+
+function isValid() {
+	var valid = true;
+
+	valid &= fieldValidation(fields.name, isNotEmpty);
+	valid &= fieldValidation(fields.email, isEmail);
+	valid &= fieldValidation(fields.subject, isNotEmpty);
+	valid &= fieldValidation(fields.message, isNotEmpty);
+	return valid;
+}
+
+$(document).ready(function (){
+	$('.submit').click(function (event){
+		var statusElm = $('.status')
+		statusElm.empty()
+		statusElm.empty()
+		if(isValid){
+			console.log('sent')
+			statusElm.append('<div>Thanks for your message </div>')
+			statusElm.append('<div>I will be in touch shortly </div>')
+		} else{
+			statusElm.append('<div>Ops, Some of your input might not valid </div>')
+			statusElm.append('<div>please try again </div>')
+		}
+	})
+})
+
+// Init App
+function init() {
+	const txtElement = document.querySelector('.txt-type');
+	const words = JSON.parse(txtElement.getAttribute('data-words'));
+	const wait = txtElement.getAttribute('data-wait');
+	// Init TypeWriter
+	new TypeWriter(txtElement, words, wait);
+}
