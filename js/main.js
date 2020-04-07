2cdf11ce1aa2fd319ec0c062b52f71db3dc38a4e
Xirui He <xih173@ucsd.edu>
2020-04-06 23:33:05 -0700

    contact

--- js/main.js
+++ js/main.js
@@ -266,60 +266,6 @@
 
 }());
 
-class TypeWriter {
-	constructor(txtElement, words, wait = 200) {
-		this.txtElement = txtElement;
-		this.words = words;
-		this.txt = '';
-		this.wordIndex = 0;
-		this.wait = parseInt(wait, 10);
-		this.type();
-		this.isDeleting = false;
-	}
-
-	type() {
-		// Current index of word
-		const current = this.wordIndex % this.words.length;
-		// Get full text of current word
-		const fullTxt = this.words[current];
-
-		// Check if deleting
-		if(this.isDeleting) {
-			// Remove char
-			this.txt = fullTxt.substring(0, this.txt.length - 1);
-		} else {
-			// Add char
-			this.txt = fullTxt.substring(0, this.txt.length + 1);
-		}
-
-		// Insert txt into element
-		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-		// Initial Type Speed
-		let typeSpeed = 100;
-
-		if(this.isDeleting) {
-			typeSpeed /= 2;
-		}
-
-		// If word is complete
-		if(!this.isDeleting && this.txt === fullTxt) {
-			// Make pause at end
-			typeSpeed = this.wait;
-			// Set delete to true
-			this.isDeleting = true;
-		} else if(this.isDeleting && this.txt === '') {
-			this.isDeleting = false;
-			// Move to next word
-			this.wordIndex++;
-			// Pause before start typing
-			typeSpeed = 500;
-		}
-
-		setTimeout(() => this.type(), typeSpeed);
-	}
-}
-
 document.addEventListener("DOMContentLoaded", function() {
 	fields.name = document.getElementById('Name');
 	fields.email = document.getElementById('email');
